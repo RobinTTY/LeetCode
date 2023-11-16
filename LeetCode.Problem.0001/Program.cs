66c00532f4a415b16608202bfc6849c8d1396b64
RobinTTY <muellerobin95@gmail.com>
2023-11-16 03:51:06 +0100

    Add TwoSum solution

---
+++ LeetCode.Problem.0001/Program.cs
@@ -0,0 +1,20 @@
+﻿var solution = new Solution();
+var result = solution.TwoSum(new[]{2, 7, 11, 15}, 9);
+result.ToList().ForEach(Console.WriteLine);
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = target - nums[i];
+            if (map.TryGetValue(num, out var index))
+                return new[] {i, index};
+            map[nums[i]] = i;
+        }
+
+        return new int[2];
+    }
+}
